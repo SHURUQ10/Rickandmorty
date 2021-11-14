04666280320d06d7fb32883882e6a59d2b012527
shroog <shroogalhwity@gmail.com>
2021-11-14 11:29:16 +0300

    Initial Commit

---
+++ Rickandmorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rickandmorty
+//
+//  Created by شروق فرج سويلم الحويطي on 09/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
